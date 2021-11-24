66c11dd60c4661bbd265e37a24a085053dc532c2
qijianguo <1633972602@qq.com>
2021-11-24 18:58:34 +0800

    update pom.xml

--- account-micro-service/account-interfaces/src/main/java/com/qijianguo/micro/services/account/interfaces/vo/TestResponse.java
+++
@@ -1,9 +0,0 @@
-package com.qijianguo.micro.services.account.interfaces.vo;
-
-import com.qijianguo.micro.services.base.model.dto.BaseResponse;
-
-/**
- * @author qijianguo
- */
-public class TestResponse extends BaseResponse {
-}
